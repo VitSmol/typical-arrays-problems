5de0c6c009bbf58827aff567e420ea80af6d8ed7
VitSmol <smolitskiy@mail.ru>
2021-08-10 09:46:05 +0300

    feat: add JS code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let min = Math.min.apply(null, array);
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let max = Math.max.apply(null, array);
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let sum = 0;
+    for (i in array) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
